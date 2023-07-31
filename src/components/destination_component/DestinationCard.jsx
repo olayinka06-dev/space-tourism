5ff80baf7ee737725477020f8f25f633ed5d80bd
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-07-31 04:54:51 -0700

    creating and editing the destination component

--- src/components/destination_component/DestinationCard.jsx
+++ src/components/destination_component/DestinationCard.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+"use client";
+import React, {useEffect } from 'react';
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 const DestinationCard = ({image, planet_name, planet_info, distance, time}) => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      delay: 100
+    })
+  }, []);
   return (
     <div className="container mx-auto">
       <div className="flex">
-        <div className="w-full md:w-1/2">
+        <div data-aos="fade-right" className="w-full md:w-1/2">
           <img className="" src={image} alt="Planets" />
         </div>
-        <div className="w-full md:w-1/2">
+        <div data-aos="fade-left" className="w-full md:w-1/2">
           <div className="">
             <h1 className="text-[40px] text-white">{planet_name}</h1>
             <p className="text-white text-opacity-30">{planet_info}</p>
