c0760c392623365f3d6f382d75a6dfee6eab6c06
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-07-31 08:38:47 -0700

    creating the pagination ball data

--- src/components/crew_component/Crew.jsx
+++ src/components/crew_component/Crew.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 const Crew = () => {
+  const pagination = [
+    {
+      ball: (
+        <span className='bg-white h-2 w-2 rounded-full'></span>
+      )
+    },
+    {
+      ball: (
+        <span className='bg-white h-2 w-2 rounded-full'></span>
+      )
+    },
+    {
+      ball: (
+        <span className='bg-white h-2 w-2 rounded-full'></span>
+      )
+    },
+    {
+      ball: (
+        <span className='bg-white h-2 w-2 rounded-full'></span>
+      )
+    },
+  ]
   return (
-    <div>Crew</div>
+    <div className="">
+      <div className=""></div>
+      <div className="">
+        {
+
+        }
+      </div>
+    </div>
   )
 }
 
