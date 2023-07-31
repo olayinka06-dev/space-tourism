1d999843408d68029d2e723fe554838a082c6a9d
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-07-31 06:48:34 -0700

    creating and editing the crewcard component

---
+++ src/components/crew_component/CrewCard.jsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const CrewCard = ({ image, planet_name, planet_info, distance, time }) => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      delay: 100,
+    });
+  }, []);
+  return (
+    <div className="container mx-auto">
+      <div className="flex">
+        <div data-aos="fade-left" className="w-full md:w-1/2">
+          <div className="">
+            <h1 className="text-[40px] text-white">{planet_name}</h1>
+            <p className="text-white text-opacity-30">{planet_info}</p>
+          </div>
+          <div className="flex">
+            <div className="">
+              <span className="text-white text-opacity-30">Avg. distance</span>
+              <h1 className="text-[40px] text-white">{distance}</h1>
+            </div>
+            <div className="">
+              <span className="text-white text-opacity-30">
+                Est. travel time
+              </span>
+              <h1 className="text-[40px] text-white">{time}</h1>
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-right" className="w-full md:w-1/2">
+          <img className="" src={image} alt="Planets" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CrewCard;
